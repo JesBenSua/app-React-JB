d8df2b2042967a4f348dfa37a88175572610f1d9
Jessi Suarez <jbenitezsuarez41@gmail.com>
2024-06-06 13:44:13 -0300

    app-intro-JB

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from './components/Header'
+import { Body } from './components/Body'
+import { Footer } from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+    <Header />
+    <div>
+      <Body />
+      <Footer />
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default App
